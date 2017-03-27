a246f0756d43e5d07cc5aa7fa429669f4760df73
dntech <dnshnayeem@gmail.com>
2017-03-27 15:17:27 -0400

    asf

--- android/richweb_tutorials/contact/index.php
+++ android/richweb_tutorials/contact/index.php
@@ -15,7 +15,7 @@
 			$query = "insert into rtutorials(name,topic,message,ip) values($nm,$tp,$msg,$ip);";
 			$rt = mysqli_query($con,$query);
 			if(!$rt){
-				echo(mysqli_error());
+				die("Error");
 			}else
 				mysqli_close($con);
 				echo("success");
